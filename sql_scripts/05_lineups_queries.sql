97ed336defcec657b13ef22dc9eb85e7be93992a
Oliver de Visser <42585140+oliverdevisser@users.noreply.github.com>
2024-09-19 22:57:55 +0200

    final commit fr: Update 05_lineups_queries.sql

--- sql_scripts/05_lineups_queries.sql
+++ sql_scripts/05_lineups_queries.sql
@@ -1,4 +1,6 @@
 --- a -----
+DROP TABLE IF EXISTS wide_table;
+CREATE TABLE wide_table AS
 WITH lineup_wide AS (
     SELECT l.game_id,
         l.team_id,
@@ -72,14 +74,17 @@ ORDER BY game_id,
     team_id,
     lineup_num,
     period;
-
+-------
 
 
 
 --- b -----
+-- gets individual stints
 DROP TABLE IF EXISTS temp_stint_accumulation;
-CREATE TEMP TABLE temp_stint_accumulation AS WITH player_stints AS (
-    SELECT gs.game_date,
+CREATE TEMP TABLE temp_stint_accumulation AS
+WITH player_stints AS (
+    SELECT
+        gs.game_date,
         gs.game_id,
         l.team_id,
         t.teamname AS team,
@@ -92,43 +97,43 @@ CREATE TEMP TABLE temp_stint_accumulation AS WITH player_stints AS (
         l.time_in,
         l.time_out,
         LEAD(l.time_in) OVER (
-            PARTITION BY l.player_id,
-            l.period,
-            l.game_id
-            ORDER BY l.time_in DESC
+            PARTITION BY l.player_id, l.game_id, l.period
+            ORDER BY l.time_in DESC, l.time_out DESC
         ) AS next_time_in
     FROM lineup l
-        JOIN game_schedule gs ON l.game_id = gs.game_id
-        JOIN team t ON l.team_id = t.teamid
-        JOIN team opp ON (
-            CASE
-                WHEN l.team_id = gs.home_id THEN gs.away_id
-                ELSE gs.home_id
-            END
-        ) = opp.teamid
-        JOIN team home ON gs.home_id = home.teamid
-        JOIN player p ON l.player_id = p.player_id
+    JOIN game_schedule gs ON l.game_id = gs.game_id
+    JOIN team t ON l.team_id = t.teamid
+    JOIN team opp ON (
+        CASE WHEN l.team_id = gs.home_id THEN gs.away_id ELSE gs.home_id END
+    ) = opp.teamid
+    JOIN team home ON gs.home_id = home.teamid
+    JOIN player p ON l.player_id = p.player_id
+    GROUP BY
+        gs.game_date, gs.game_id, l.team_id, t.teamname, opp.teamname, home.teamname, 
+        p.first_name, p.last_name, l.period, l.time_in, l.time_out, l.player_id, l.game_id
 ),
 stint_grouping AS (
     SELECT *,
-        CASE
-            WHEN next_time_in IS NOT NULL
-            AND next_time_in < time_out THEN 1
-            ELSE 0
-        END AS stint_break
+        LAG(CASE 
+                WHEN next_time_in IS NOT NULL AND next_time_in < time_out THEN 1
+                ELSE 0
+            END, 1, 0) OVER (
+            PARTITION BY player_name, game_id, period
+            ORDER BY time_in DESC, time_out DESC
+        ) AS stint_break
     FROM player_stints
 ),
-stint_lagged AS (
+stint_numbers AS (
     SELECT *,
-        LAG(stint_break, 1, 0) OVER (
-            PARTITION BY player_name,
-            period,
-            game_date
-            ORDER BY time_in DESC
-        ) AS lagged_stint_break
+        SUM(stint_break) OVER (
+            PARTITION BY player_name, game_id, period
+            ORDER BY time_in DESC, time_out DESC
+            ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+        ) + 1 AS stint_number
     FROM stint_grouping
 )
-SELECT game_date,
+SELECT
+    game_date,
     game_id,
     team_id,
     team,
@@ -137,50 +142,49 @@ SELECT game_date,
     period,
     time_in,
     time_out,
-    SUM(lagged_stint_break) OVER (
-        PARTITION BY player_name,
-        period,
-        game_date
-        ORDER BY time_in DESC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
-    ) + 1 AS stint_number
-FROM stint_lagged;
-DROP TABLE IF EXISTS stints_results; 
+    stint_number
+FROM stint_numbers
+ORDER BY game_date, team, player_name, game_id, period, time_in DESC, time_out DESC;
+
+-- combines stints to get final results
+DROP TABLE IF EXISTS stints_results;
 CREATE TABLE stints_results AS
-SELECT game_date,
-    game_id,
+SELECT
+    game_date,
     team,
     opponent,
     player_name,
     period,
     stint_number,
-    TO_CHAR(MIN(time_in) * INTERVAL '1 second', 'MI:SS') AS stint_start_time,
-    TO_CHAR(MAX(time_out) * INTERVAL '1 second', 'MI:SS') AS stint_end_time
+    TO_CHAR(MAX(time_in) * INTERVAL '1 second', 'MI:SS') AS stint_start_time,
+    TO_CHAR(MIN(time_out) * INTERVAL '1 second', 'MI:SS') AS stint_end_time
 FROM temp_stint_accumulation
-GROUP BY game_date,
+GROUP BY
+    game_date,
     game_id,
     team,
     opponent,
     player_name,
     period,
     stint_number
-ORDER BY game_date,
-    team,
-    player_name,
-    period,
-    stint_number;
+ORDER BY
+    game_date, team, player_name, period, stint_number;
+
+-- get seconds of each stint for later use
 DROP TABLE IF EXISTS temp_stint_durations;
 CREATE TEMP TABLE temp_stint_durations AS
-SELECT sr.*,
-    ABS(MIN(tsa.time_in) - MAX(tsa.time_out)) AS stint_seconds
+SELECT
+    sr.*,
+    (MAX(tsa.time_in) - MIN(tsa.time_out)) AS stint_seconds
 FROM stints_results sr
-    JOIN temp_stint_accumulation tsa ON sr.game_date = tsa.game_date
-    AND sr.game_id = tsa.game_id 
-    AND sr.team = tsa.team
-    AND sr.player_name = tsa.player_name
-    AND sr.period = tsa.period
-    AND sr.stint_number = tsa.stint_number
-GROUP BY sr.game_date,
-    sr.game_id,
+JOIN temp_stint_accumulation tsa ON
+    sr.game_date = tsa.game_date AND
+    sr.team = tsa.team AND
+    sr.player_name = tsa.player_name AND
+    sr.period = tsa.period AND
+    sr.stint_number = tsa.stint_number
+GROUP BY
+    sr.game_date,
     sr.team,
     sr.opponent,
     sr.player_name,
@@ -190,19 +194,20 @@ GROUP BY sr.game_date,
     sr.stint_end_time;
 
 
------ c -------
-SELECT player_name,
-    team,
-    ROUND(COUNT(*)::NUMERIC / COUNT(DISTINCT game_date), 2) AS average_stints_per_game,
-    ROUND(CAST(AVG(stint_seconds) AS NUMERIC), 2) AS avg_stint_length_seconds
-FROM temp_stint_durations
-GROUP BY player_name,
-    team
-ORDER BY team,
-    avg_stint_length_seconds DESC,
-    average_stints_per_game DESC
-LIMIT 1000;
 
+----- c -------
+DROP TABLE IF EXISTS stints_results_averages;
+CREATE TABLE stints_results_averages AS 
+    SELECT player_name,
+        team,
+        ROUND(COUNT(*)::NUMERIC / COUNT(DISTINCT game_date), 2) AS average_stints_per_game,
+        ROUND(CAST(AVG(stint_seconds) AS NUMERIC), 2) AS avg_stint_length_seconds
+    FROM temp_stint_durations
+    GROUP BY player_name,
+        team
+    ORDER BY team,
+        avg_stint_length_seconds DESC,
+        average_stints_per_game DESC;
 
 ----- d --------
 DROP TABLE IF EXISTS final_summary;
@@ -233,7 +238,7 @@ stint_durations_with_results AS (
     SELECT tsd.*,
         gr.result
     FROM temp_stint_durations tsd
-        JOIN game_results gr ON tsd.game_id = gr.game_id
+        JOIN game_results gr ON tsd.game_date = gr.game_date
         AND tsd.team = gr.team
 )
 SELECT player_name,
@@ -359,3 +364,11 @@ FROM stint_durations_with_results
 GROUP BY player_name,
     team
 ORDER BY player_name;
+
+
+
+
+
+
+
+
